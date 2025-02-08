7f42e9a0efca2471b6c2380a7f4f620a85d8f4c2
CodeCascade10 <kausiknaskar10@gmail.com>
2025-02-08 19:52:02 +0000

    datatypes update

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let score=13
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+//"33"=>33
+//"33abc"=>NaN
+// true=>1; false=>0
+let isLoggedIn = 1;
+let booleanisLoggedIn=Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+//1=>true;0=>false
+//""=>false
+//'kausik'=>true
+let aNumber=31
+let stringNumber=string(aNumber)
+console.log(stringNumber);
