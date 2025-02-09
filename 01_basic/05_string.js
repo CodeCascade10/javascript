964cc13c5e21a8d0bd5fc28afbec64409895bf89
CodeCascade10 <kausiknaskar10@gmail.com>
2025-02-09 17:33:00 +0000

    math&num

---
+++ 01_basic/05_string.js
@@ -0,0 +1,3 @@
+const name = "kausik";
+const repoCount = 20;
+console.log(`Hello, my name is ${name} and my repo count is ${repoCount}`);
---
+++ 01_basic/06_nums_math.js
@@ -0,0 +1,20 @@
+/*const score=480
+
+
+const balance= new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber=23.6758
+
+console.log(otherNumber.toPrecision(3));
+const hundreds=100000
+console.log(hundreds.toLocaleString('en-IN'));*/
+/////////=+++++ Maths ++//////////////
+//console.log(Math);
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*10)+1);
+console.log(Math.floor(Math.random()*(max-min+1)+min))
